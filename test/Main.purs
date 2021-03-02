894b1142e17551cbc74a48dfb584ed938857c81f
Grzegorz Uriasz <gorbak25@gmail.com>
2021-03-02 11:48:08 +0100

    More tests

--- test/Main.purs
+++ test/Main.purs
@@ -38,6 +38,8 @@ import Erlang.Invoke
 import Aeser.Api.Encoder.Tests
 import Aeser.Rlp.Tests
 import Aeser.Chain.Objects.Tests
+import Aeser.Contract.Code.Tests
+import Aeser.Contract.Code
 
 assertOk f args = do
   r <- exec f args
@@ -107,3 +109,25 @@ main = unsafePartial $
             assertOk erlps__tag_fail_test__0 []
         it "erlps__vsn_fail_test__0" do
             assertOk erlps__vsn_fail_test__0 []
+      describe "AeserContractCodeTests" do
+        it "vsn_1_test" do
+            assertOk erlps__vsn_1_test__0 []
+        it "vsn_1_test bis" do
+            let expected = bin [244,70,1,160,48,58,125,237,188,44,120,213,52,155,92,4,213,8,157,236,198,161,240,9,117,91,60,167,64,44,67,82,145,174,238,243,197,196,128,128,128,128,138,68,85,77,77,89,32,67,79,68,69]
+            a <- exec erlps__vsn_1_test__0 []
+            b <- unpackOk a
+            testExecOk expected erlps__serialize__2 [b, toErl 1]
+        it "vsn_2_test" do
+            assertOk erlps__vsn_2_test__0 []
+        it "vsn_2_test bis" do
+            let expected = bin [222,70,2,132,1,2,3,4,197,196,128,128,128,128,138,68,85,77,77,89,32,67,79,68,69,133,51,46,49,46,52]
+            a <- exec erlps__vsn_2_test__0 []
+            b <- unpackOk a
+            testExecOk expected erlps__serialize__2 [b, toErl 2]
+        it "vsn_3_test" do
+            assertOk erlps__vsn_3_test__0 []
+        it "vsn_3_test bis" do
+            let expected = bin [248,60,70,3,160,48,58,125,237,188,44,120,213,52,155,92,4,213,8,157,236,198,161,240,9,117,91,60,167,64,44,67,82,145,174,238,243,198,197,128,128,0,128,128,138,68,85,77,77,89,32,67,79,68,69,133,51,46,49,46,52,1]
+            a <- exec erlps__vsn_3_test__0 []
+            b <- unpackOk a
+            testExecOk expected erlps__serialize__2 [b, toErl 3]
